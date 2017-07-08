3a69f1cc7a72934124eb1932863d14d95da81ecd
Parthi <parthiban.sudhaman@gmail.com>
2017-07-08 20:28:11 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+    <h1>Artemis</h1>
+  </div>, 
+  document.getElementById('root')  
+);
+registerServiceWorker();
